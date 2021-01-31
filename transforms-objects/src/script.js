5c426521bda27e40601a82d2b0cf2bc75e3f1fc9
parkerziegler <parker.ziegler@formidable.com>
2021-01-31 12:43:32 -0700

    feat: add basic transform example

---
+++ transforms-objects/src/script.js
@@ -0,0 +1,68 @@
+import * as THREE from "three";
+
+import "./style.css";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "#7860ed" })
+);
+
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "#7860ed" })
+);
+
+cube2.position.set(-2, 0, 0);
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "#7860ed" })
+);
+
+cube3.position.set(2, 0, 0);
+group.add(cube3);
+
+const axesHelpner = new THREE.AxesHelper();
+scene.add(axesHelpner);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
